3a2d8da10decdc433c38236f3c5364220f0c07a6
Shantanu Tripathi <89775268+itzz-shantanu@users.noreply.github.com>
2023-08-17 00:10:28 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -3,5 +3,30 @@ using namespace std;
 int main()
 {
     cout << "Namaste Duniya" << endl;
+    int a = 123;
+    char b = '%';
+    bool c = false;
+    float d = 1.2;
+    double e = 1.23;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+    cout << sizeof(a) << sizeof(b) << sizeof(c) << sizeof(d) << sizeof(e) << endl;
+    int f = 'b';
+    char ch = 98;
+    cout << f << endl;
+    cout << ch << endl;
+    char chh = 123456; // 123456%256 =64 and its ascii value is @
+    cout << chh << endl;
+    int g = -5;
+    cout << g << endl;
+    cout << 2 / 5 << endl;
+    cout << 2.0 / 5 << endl;
+    cout << 2 / 5.0 << endl;
+    cout << -2.0 / 5 << endl;
+    bool p = (d != e);
+    cout << p << endl;
     return 0;
 }
