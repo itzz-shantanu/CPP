578ecc2fb37f2c295675dc054053b1b2ec879025
Shantanu Tripathi <89775268+itzz-shantanu@users.noreply.github.com>
2023-12-18 23:55:35 +0530

    Add files via upload

---
+++ 38.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cin >> a;
+    cin >> b;
+
+    char op;
+    cin >> op;
+
+    switch (op)
+    {
+    case '+':
+        cout << a + b << endl;
+        break;
+    case '-':
+        cout << a - b << endl;
+        break;
+    case '/':
+        cout << a / b << endl;
+        break;
+    case '*':
+        cout << a * b << endl;
+        break;
+
+    default:
+        cout << "invalid" ;
+        break;
+    }
+
+    return 0;
+}
